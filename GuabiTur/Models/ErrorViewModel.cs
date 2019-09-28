b3f5b0f97d719c2e31c3fe5bbe6efe4663b5dfdf
JefferssonSemin <jeffesemin@hotmail.com>
2019-09-28 17:43:25 -0300

    adicionado escopo do projeto

---
+++ GuabiTur/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GuabiTur.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
